8b652bb868f1cc71882bdb6c85c480dd85c03c06
haxkd <abulhax@gmail.com>
2024-03-14 11:47:57 +0530

    fix navigation issue and back to top

--- src/Components/Includes/Footer.jsx
+++ src/Components/Includes/Footer.jsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+        window.removeEventListener('scroll', toggleVisibility);
+    };
+}, []);
+
+const toggleVisibility = () => {
+    if (window.pageYOffset > 100) {
+        setIsVisible(true);
+    } else {
+        setIsVisible(false);
+    }
+};
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
   return (
     <>
-    <footer className="footer mt-3 py-3 bg-light text-center">
-            <div className="container">
-                <span className="text-muted">© 2024 Product Manager</span>
-            </div>
-        </footer>
+      <footer className="footer mt-3 py-3 bg-light text-center">
+        <div className="container">
+          <span className="text-muted">© 2024 Product Manager</span>
+        </div>
+        <div
+          className={`back-to-top ${isVisible ? 'visible' : 'hidden'}`}
+          onClick={scrollToTop}
+        >
+          <span>↑</span>
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Includes/Header.jsx
+++ src/Components/Includes/Header.jsx
@@ -1,9 +1,15 @@
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
 function Header() {
+  const location = useLocation();
+  useEffect(() => {
+    // Whenever the location changes (i.e., navigating to a new page), scroll to the top
+    window.scrollTo(0, 0);
+}, [location]);
   return (
     <Navbar expand="lg" className="navbar-dark">
       <Container>
